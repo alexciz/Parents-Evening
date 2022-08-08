f3ddefa74e7056f524ddcc63dedffb7491d3a19e
alexciz <83537331+alexciz@users.noreply.github.com>
2022-08-08 21:29:44 +0100

    fdf

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import sys
 import msvcrt
 import time
 
-slots = [["1", "2", "3", "4", "5", "6", "7", "8", "9"], ["1", "2", "3", "4", "5", "6", "7", "8", "9"], ["1", "2", "3", "4", "5", "6", "7", "8", "9"]]
-
+p_num = 1
 
 def clear():
     # \033[H homes the cursor
@@ -19,7 +18,7 @@ def hidden_input():
         if x == '\r' or x == '\n':
             break
         print('*', end='', flush=True)
-        passwor +=x
+        passwor += x
     return passwor
 
 def user_selection():
@@ -27,64 +26,54 @@ def user_selection():
     global user
     t_password = "T_auth"
     user = "p"
-    us_loop = 1
-    p_attempts = 0
-    
-    while us_loop == 1:
+    remaining_attempts = 5
+
+    while True:
         clear()
         u_input = input("Are you a parent?\n")
         if u_input == "No" or u_input == "no" or u_input == "n" or u_input == "N":
-            us_loop = 0
             clear()
             print("Teacher Login")
             while True:
                 print("Password:")
                 hidden_input()
-                if passwor == str(t_password):
+                if passwor == t_password:
                     user = "t"
-                    print("\nCorrect")
+                    print("\nAccess Granted")
                     break
                 else:
-                    p_attempts += 1
-                    remaining_attempts = 5-int(p_attempts)
+                    remaining_attempts -=1
                     clear()
                     print("Incorrect Password\nYou have", remaining_attempts ,"attempts remaining.")
 
                 if remaining_attempts == 0:
                     print("Password Attempt Limit Exceeded")
                     exit()
-        
+            break
         elif u_input == "Yes" or u_input == "yes" or u_input == "y" or u_input == "Y":
             user = "p"
-            us_loop = 0
+            break
         else:
             print("Please enter Yes or No.")
             time.sleep(0.7)
 
 def p_input():
 
-    global x
-    global y
-    global d
+    global p_num
     global preferences
     global preferenced
     global preferencet
-    #global slots
 
-    x = 1
-    y = 1
-    d = 1
     preferences = 0
 
-    #slots = [["1", "2", "3", "4", "5", "6", "7", "8", "9"], ["1", "2", "3", "4", "5", "6", "7", "8", "9"], ["1", "2", "3", "4", "5", "6", "7", "8", "9"]]
     preferenced = ["1", "2"]
     preferencet = ["1", "2"]
 
     
-    if x < 24:
+    if p_num < 24:
         clear()
         print("Crawdale Parents Eveining Booking Portal\n")
-        print("Hello! You are Parent " + str(x) +  ".\n")
+        print("Hello! You are Parent " + str(p_num) +  ".\n")
         
         for preferences in range(2):
 
@@ -94,81 +83,106 @@ def p_input():
                 print("Please enter your second booking choice.\n")
 
 
-            while d == 1:
+            while True:
                 day = str(input("What day would you like your appointment on? Please enter a number from 1-3.\n"))
 
                 if day == "1":
-                    d = 0
+                    break
                 elif day == "2":
-                    d = 0
+                    break
                 elif day == "3":
-                    d = 0
+                    break
                 else:
                     clear()
                     print("Please enter 1, 2 or 3 only.\n")
                 
-            d = 1
             day = int(day)
-            day -= 1
-            while y == 1:
+            while True:
                 print("\nPlease select a time from the following list:")
                 print("1 = 17:00\n2 = 17:20\n3 = 17:40\n4 = 18:00\n5 = 18:20\n6 = 18:40\n7 = 19:00\n8 = 19:20\n9 = 19:40")
                 time = str(input())
-                y = 0
                 if time == "1":
-                    time = 0
+                    break
                 elif time == "2":
-                    time = 1
+                    break
                 elif time == "3":
-                    time = 2
+                    break
                 elif time == "4":
-                    time = 3
+                    break
                 elif time == "5":
-                    time = 4
+                    break
                 elif time == "6":
-                    time = 5
+                    break
                 elif time == "7":
-                    time = 6
+                    break
                 elif time == "8":
-                    time = 7
+                    break
                 elif time == "9":
-                    time = 8
+                    break
                 else:
-                    print("Please enter a valid time.")
-                    y = 1
+                    clear()
+                    print("\nPlease enter a number from 1-9.")
                 preferenced[preferences] = day
                 preferencet[preferences] = time
-                print("\n")
-                #print(preferenced[0])
-                #print(preferenced[1])
-                #print(preferencet[0])
-                #print(preferencet[1])
                 
                 preferences += 1
             
 
-            y = 1
+        p_num += 1
 
-        #y = 1
-        x += 1
+        p_num_print = (str(p_num)+ "\n")
+ 
+        f = open("bookings.txt", "r")
+        p_lines = f.readlines()
+        p_lines[0] = p_num_print
 
-        f = open("bookings.txt", "wt")
-        f.write(str(x))
+        f = open("bookings.txt", "w")
+        f.writelines(p_lines)
         f.close()
+
+def booking_verify():
+    global p1_line
+    global p2_line
+    
+    b_time = "17:00"
+    p1_line = ((int(preferenced[0]) * 10) + int(preferencet[0]))-7
+    p2_line = ((int(preferenced[1]) * 10) + int(preferencet[1]))-7
+
+    f = open("bookings.txt", "r")
+    
+    if f.readline(p1_line)[0] != "P":
+        lines = f.readlines()
+        lines[(p1_line - 1)] = "P"+p_num+"\n"
+        
+        f = open("bookings.txt", "w")
+        f.writelines(lines)
+        f.close()
+        if preferencet[0] == 1:
+            b_time = "17:00"
+        
+        print("Preference 1 Booked\nYour booking is on Day", preferenced[0], "at", b_time + ".")
+
 
 
 
 if exists("bookings.txt") == True:
     f = open("bookings.txt", "rt")
-    x = int(f.read())
+    p_num = int(f.readline(1))
     f.close()
-
 else:
     f = open("bookings.txt", "x")
     f = open("bookings.txt", "wt")
-    f.write(str(x))
+    f.write(str(p_num))
+    f.write("\n\nDay 1 Bookings\n1\n2\n3\n4\n5\n6\n7\n8\n9\nDay 2 Bookings\n1\n2\n3\n4\n5\n6\n7\n8\n9\nDay 3 Bookings\n1\n2\n3\n4\n5\n6\n7\n8\n9")
     f.close()
 
+
+clear()
+print("Welcome to Crawdale Parents Evening System\n")
+time.sleep(1.5)
+
 user_selection()
+
 if user == "p":
     p_input()
+    booking_verify()
