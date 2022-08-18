59da20b4b1ecf4df9af5dddc1b3897031d4537df
alexciz <83537331+alexciz@users.noreply.github.com>
2022-08-18 22:10:12 +0100

    Finished + Cancel Apt, Print Timetable, Stop Menu, File is read to variable

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from genericpath import exists
+from importlib import reload
 import msvcrt
 import time
-import linecache
 import os
 
-
 p_num = 1
+slots = 0
+
+if exists("bookings.txt") == True:
+    f = open("bookings.txt", "rt")
+    p_num = int(f.readline(1))
+    f.close()
+else:
+    f = open("bookings.txt", "x")
+    f = open("bookings.txt", "wt")
+    f.write(str(p_num))
+    f.write("\n\nDay 1 Bookings\n1.  \n2.  \n3.  \n4.  \n5.  \n6.  \n7.  \n8.  \n9.  \nDay 2 Bookings\n1.  \n2.  \n3.  \n4.  \n5.  \n6.  \n7.  \n8.  \n9.  \nDay 3 Bookings\n1.  \n2.  \n3.  \n4.  \n5.  \n6.  \n7.  \n8.  \n9.  ")
+    f.close()
+
+f = open("bookings.txt", "rt")
+f.seek(0)
+slots = f.readlines()
+f.close()
 
 def clear():
     os.system('cls||clear')
@@ -27,6 +43,7 @@ def user_selection():
     t_password = "T_auth"
     user = "p"
     remaining_attempts = 5
+    reset = f"\033[0m"
 
     while True:
         clear()
@@ -39,7 +56,7 @@ def user_selection():
                 hidden_input()
                 if passwor == t_password:
                     user = "t"
-                    print("\nAccess Granted")
+                    print("\033[1;32m \nAccess Granted" + reset)
                     break
                 else:
                     remaining_attempts -=1
@@ -56,6 +73,101 @@ def user_selection():
         else:
             print("Please enter Yes or No.")
             time.sleep(0.7)
+
+def print_timetable():
+    time.sleep(0.5)
+    clear()
+    print("Parents Evening Bookings\n")
+    print(*slots[3:])
+    while True:
+        stop = input("\nPlease enter 'stop' to exit teacher mode.\n")
+        if stop == "stop":
+            break
+        
+    
+def p_menu():
+    clear()
+    while True:
+        p_option = input("Please select what you would like to do by entering the corresponding number.\n1 = Book new appointment\n2 = Cancel an appointment\n")
+        if p_option == "1":
+            p_option = "new_apt"
+            break
+        elif p_option == "2":
+            p_option = "cancel_apt"
+            break
+        else:
+            clear()
+            print("Please enter 1 or 2 only.\n")
+    
+    return p_option
+
+def cancel_apt():
+    name_conf = 1
+    reset = f"\033[0m"
+
+    clear()
+    while True:
+        cancel_day = input("What day is the appointment on that you would like to cancel? Please enter 1, 2 or 3.\n")
+        if cancel_day == "1":
+            break
+        elif cancel_day == "2":
+            break
+        elif cancel_day == "3":
+            break
+        else:
+            clear()
+            print("Please enter 1, 2 or 3 only.\n")
+    clear()
+    while True:
+        
+        cancel_time = input("Please select the time of your appoitment from the following list:\n1 = 17:00\n2 = 17:20\n3 = 17:40\n4 = 18:00\n5 = 18:20\n6 = 18:40\n7 = 19:00\n8 = 19:20\n9 = 19:40\n")
+        
+        if cancel_time == "1":
+            break
+        elif cancel_time == "2":
+            break
+        elif cancel_time == "3":
+            break
+        elif cancel_time == "4":
+            break
+        elif cancel_time == "5":
+            break
+        elif cancel_time == "6":
+            break
+        elif cancel_time == "7":
+            break
+        elif cancel_time == "8":
+            break
+        elif cancel_time == "9":
+            break
+        else:
+            clear()
+            print("\nPlease enter a number from 1-9.")
+    
+    cancel_index = ((int(cancel_day)*10)+int(cancel_time))-8
+    
+    clear()
+
+    while True:
+        
+        cancel_name = input("Please enter the name on the appointment you would like to cancel:\n")
+        if slots[cancel_index][3:] == cancel_name+"\n":
+            slots[cancel_index] = cancel_time+".  \n"
+
+            print("\033[1;32mAppointment Cancelled" + reset)
+            time.sleep(1.5)
+            break
+        else:
+            name_conf += 1
+
+        if name_conf == 3:
+            clear()
+            print("Name does not match.\nName Confirmation Attempt Limit Exceeded")
+            exit()
+        else:
+            clear()
+            print("Name does not match.\nYou have one more attempt.\n")
+
 
 def p_input():
 
@@ -105,7 +217,7 @@ def p_input():
             while True:
                 print("\nPlease select a time from the following list:")
                 print("1 = 17:00\n2 = 17:20\n3 = 17:40\n4 = 18:00\n5 = 18:20\n6 = 18:40\n7 = 19:00\n8 = 19:20\n9 = 19:40")
-                time = str(input())
+                time = input()
                 if time == "1":
                     break
                 elif time == "2":
@@ -135,20 +247,13 @@ def p_input():
 
         p_num += 1
 
-        p_num_print = (str(p_num)+ "\n")
- 
-        f = open("bookings.txt", "r")
-        p_lines = f.readlines()
-        p_lines[0] = p_num_print
+        p_num_print = str(p_num)+ "\n"
 
-        f = open("bookings.txt", "w")
-        f.writelines(p_lines)
-        f.close()
+        slots[0] = p_num_print
+
     else:
         clear()
         print("All parents have booked.")
-        f = open("bookings.txt", "r")
-        f.close()
         exit()
 
 def booking_verify():
@@ -158,21 +263,14 @@ def booking_verify():
     b_pref =  0
     b_day = 0
     b_time = 0
-    p1_line = ((preferenced[0] * 10) + preferencet[0])-7
-    p2_line = ((preferenced[1] * 10) + preferencet[1])-7
+    p1_index = ((preferenced[0] * 10) + preferencet[0])-8
+    p2_index = ((preferenced[1] * 10) + preferencet[1])-8
     next_day = 0
     booked = False
 
-    f = open("bookings.txt", "rt")
+    if slots[p1_index][3] == " ":
+        slots[p1_index] = str(preferencet[0])+". " + p_name + "\n"
 
-    if linecache.getline("bookings.txt", p1_line)[3] == " ":
-        f.seek(0)
-        lines = f.readlines()
-        lines[(p1_line - 1)] = str(preferencet[0])+". " + p_name + "\n"
-
-        f = open("bookings.txt", "wt")
-        f.writelines(lines)
-        f.close()
         b_pref = "1"
         b_day = preferenced[0]
 
@@ -195,14 +293,8 @@ def booking_verify():
         elif preferencet[0] == 9:
             b_time = "19:40"
        
-    elif linecache.getline("bookings.txt", p2_line)[3] == " ":
-        f.seek(0)
-        lines = f.readlines()
-        lines[(p2_line - 1)] = str(preferencet[1]) + ". " + p_name + "\n"
-
-        f = open("bookings.txt", "wt")
-        f.writelines(lines)
-        f.close()
+    elif slots[p2_index][3] == " ":
+        slots[p2_index] = str(preferencet[1]) + ". " + p_name + "\n"
 
         b_pref = "2"
         b_day = preferenced[1]
@@ -241,45 +333,30 @@ def booking_verify():
             start_line = (int(b_prefd)*10)-8
             end_line = start_line + 10
             
-            f = open("bookings.txt", "rt")
 
-            print_lines = f.readlines()[start_line:end_line]
-            f.close()
             while True:
                 clear()
                 print("Please select an available slot from the following list for the selected day.\nSlots without a name are available.\n")
-                print(*print_lines)
+                print(*slots[start_line:end_line])
                 b_preft = input("Please select an available slot by entering its number.\nIf none of the slots suit you are none are available please enter 'next'.\n")
                 
                 if b_preft.isnumeric() == True: 
-                    bb_line = ((int(b_prefd)*10)+int(b_preft))-7
-                    
-
-                f = open("bookings.txt", "rt")
-                
+                    bb_index = ((int(b_prefd)*10)+int(b_preft))-8
 
                 if b_preft == "next":
                     clear()
                     next_day = 1
                     break
-                    f.close()
                 elif b_preft.isnumeric() == False:
                     print("\nPlease enter a number from 1-9 or 'next' only.")
                     time.sleep(1.7)
-                    f.close()
                 elif int(b_preft) not in range(1,10):
                     print("\nPlease enter a number from 1-9 or 'next' only.")
                     time.sleep(1.7)
-                    f.close()
-                elif linecache.getline("bookings.txt", bb_line)[3] == " ":
+                elif slots[bb_index][3] == " ":
                     b_pref = "3"
-                    f.seek(0)
-                    lines = f.readlines()
-                    lines[(bb_line - 1)] = str(b_preft) + ". " + p_name + "\n"
+                    slots[bb_index] = str(b_preft) + ". " + p_name + "\n"
 
-                    f = open("bookings.txt", "wt")
-                    f.writelines(lines)
-                    f.close()
                     b_day = b_prefd
 
                     if b_preft == "1":
@@ -315,26 +392,42 @@ def booking_verify():
 
     print("Preference " + b_pref + " Booked")
     print("Your booking is on Day " + str(b_day) + " at " + b_time + ".")
+    time.sleep(2)
     
-        
-if exists("bookings.txt") == True:
-    f = open("bookings.txt", "rt")
-    p_num = int(f.readline(2))
-    f.close()
-else:
-    f = open("bookings.txt", "x")
-    f = open("bookings.txt", "wt")
-    f.write(str(p_num))
-    f.write("\n\nDay 1 Bookings\n1.  \n2.  \n3.  \n4.  \n5.  \n6.  \n7.  \n8.  \n9.  \nDay 2 Bookings\n1.  \n2.  \n3.  \n4.  \n5.  \n6.  \n7.  \n8.  \n9.  \nDay 3 Bookings\n1.  \n2.  \n3.  \n4.  \n5.  \n6.  \n7.  \n8.  \n9.  ")
-    f.close()
+def stop():
+    clear()
+    while True:
+        stop = input("Would you like to stop the program? Please enter yes or no.\n")
+        if stop == "Yes" or stop == "yes" or stop == "Y" or stop == "YES" or stop == "y":
+            clear()
 
+            f = open("bookings.txt", "wt")
+            f.writelines(slots)
+            f.close()
 
-clear()
-print("Welcome to Crawdale Parents Evening System\n")
-time.sleep(1.5)
+            print("Goodbye!\n")
+            time.sleep(0.5)
+            print("██████╗░██╗░░░██╗███████╗\n██╔══██╗╚██╗░██╔╝██╔════╝\n██████╦╝░╚████╔╝░█████╗░░\n██╔══██╗░░╚██╔╝░░██╔══╝░░\n██████╦╝░░░██║░░░███████╗\n╚═════╝░░░░╚═╝░░░╚══════╝")
+            exit()
+        elif stop == "No" or stop == "no" or stop == "n" or stop == "NO" or stop == "N":
+            break
+        else:
+            clear()
+            print("Please enter yes or no only.\n")
 
-user_selection()
+while True:
+    clear()
+    print("Welcome to Crawdale Parents Evening System\n")
+    time.sleep(1.5)
 
-if user == "p":
-    p_input()
-    booking_verify()
+    user_selection()
+
+    if user == "p":
+        if p_menu() == "new_apt":
+            p_input()
+            booking_verify()
+        else:
+            cancel_apt()
+    else:
+        print_timetable()
+    stop()
